221ce427d9240435120c1058da80d83658a330a3
Taeil Kwak <taeil2@gmail.com>
2020-04-20 16:20:50 -0500

    turtle filter, map, foreach

--- functions.js
+++ functions.js
@@ -32,10 +32,37 @@ const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 // We use your `filter` function here, capturing a new array into `filteredNames`
 // comprised of names that only begin with 'R'
 const filteredNames = filter(myNames, function(name) {
-    // This is known as a "predicate function" - it's a function that 
+    // This is known as a "predicate function" - it's a function that
     // only returns a boolean
     return name[0] === 'R';
 });
 
 console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
+
+
+
+
+function hazardWarningCreator(typeOfWarning) {
+    let warningCounter = 0;
+    return function(location) {
+        warningCounter++;
+        console.log(`Danger! There is a ${typeOfWarning} hazard at ${location}!`);
+        if (warningCounter === 1) {
+            console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time today!`);
+        } else {
+            console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} times today!`);
+        }
+
+    }
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const deerWarning = hazardWarningCreator('Deer crossing the tracks');
+const slipperyWarning = hazardWarningCreator('Road is slippery');
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+deerWarning('Main St and Broadway Ave');
+slipperyWarning('Augusta Ave and Lake St');
+slipperyWarning('Greenbay Rd and Hawthorn Ave');
